f3ef73f5d1f1e00eb04a2a156aa739625f781945
student <derrien.maxime77@gmail.com>
2023-09-07 13:02:59 +0200

    finished basic test_klt node

--- rpg_klt/src/test_klt.cpp
+++ rpg_klt/src/test_klt.cpp
@@ -6,34 +6,49 @@
 #include "klt_msg/msg/ftr.hpp"
 #include "klt_msg/msg/feature_list.hpp"
 
+#define WIDTH 640
+#define HEIGHT 600
 
-class ImageNode : public rclccp::Node 
+class ImageNode : public rclcpp::Node 
 {
 	public :
-		ImageNode(){
-			cv::Mat img = cv::imread("./img/concorde.jpg");
+		ImageNode() : Node("test_klt") {
+			cv::Mat img_grey = cv::imread("./img/concorde.jpg", cv::IMREAD_GRAYSCALE);
 			cv::Mat grey;
-			cv::cvtColor(img, grey, COLOR_BGR2GRAY);
+			cv::resize(img_grey, grey, cv::Size(WIDTH,HEIGHT));
 			cv::namedWindow("test", cv::WINDOW_AUTOSIZE);
 			cv::imshow("test", grey);
-			cv::watKey(0);
+			cv::waitKey(0);
 			cv::destroyWindow("test");
-
+			this->imageConverter(grey);
+			img.height = HEIGHT;
+			img.width = WIDTH;
 			pub = this->create_publisher<sensor_msgs::msg::Image>("klt/image", 10);
+			this->loop();
 		}
 
 	private :
 		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub;
-		sensor_msgs::msg::Image img();
+		sensor_msgs::msg::Image img;
 		void imageConverter(cv::Mat grey){
-			for(){
-				
+			for(int x =0; x < WIDTH ;x++ ){
+				for(int y = 0; y<HEIGHT ; y++){
+					this->img.data[x,y] = grey.at<uint8_t>(x,y);
+				}
 			}
-		
+			return;
 		}
-		void loop();
 
-}
+		void loop(){
+			int n;
+			while(true){
+				std::cin >> n; //just to wait
+				pub->publish(img);	
+			}
+			return;
+		}
+
+};
 
 int main(int argc, char* argv[]){
 	rclcpp::init(argc, argv);
